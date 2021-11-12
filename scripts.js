0b55029676076b23d3e85f6f8d8032ba1c49480c
WaltVenditi <waltvenditti@gmail.com>
2021-11-12 18:29:17 -0500

    working on logic

--- scripts.js
+++ scripts.js
@@ -37,22 +37,48 @@ const Player = function(shape) {
 };
 
 
-//module to control game flow
-const gameController = (function(player1, player2) {
+const gameController = (function() {
+    let player1, player2;
     let winner = 'none';
     let currentPlayer;
+
+
+    for (let i = 0; i < gameBoard.getBoardState; i++) {
+        let x = `p${i}`;
+        box = document.querySelector(x);
+        box.addEventListener('click', () => {
+            
+        });
+    }
+    
+
+    const enterPlayers = function(p1, p2) {
+        player1 = p1;
+        player2 = p2;
+    };
 
     coinFlip = Math.floor(Math.random()*2) + 1;
     if (coinFlip === 1) currentPlayer = player1;
     else currentPlayer = player2;
-    /*
-    while (winner === 'none') {
+
+    let j = 0;
+    while (winner === 'none' && j < 9) {
         //player picks square
         //check for a winner
         if (currentPlayer === player1) currentPlayer = player2;
         else currentPlayer = player1;
-        break; ///!!!!!!!!!!!!!!!!!!!!!!REMOVE!!!!
+        j++;
     }
-    */
+    return {enterPlayers};
 })();
 
+let updateWebpage = (function() {
+    const updateBoard = function(currentBoard) {
+        for (let i = 0; i < currentBoard.length; i++) {
+            let x = `#p${i}`;
+            box = document.querySelector(x);
+            box.textContent = currentBoard[i];
+        }
+    }
+    return {updateBoard};
+})();
