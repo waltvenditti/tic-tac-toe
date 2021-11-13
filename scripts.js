19b6b291fa0263d545404aecd8a5744528301889
WaltVenditi <waltvenditti@gmail.com>
2021-11-13 09:53:01 -0500

    worked on JS

--- scripts.js
+++ scripts.js
@@ -4,13 +4,13 @@ const gameBoard = (function() {
     let board = ['','','','','','','','',''];
 
     const pickSquare = function(player, square) {
-        if (board[square] !== '') return;
+        if (board[square] !== '') return false;
 
         if (player === 'X') {
             board[square] = 'X';
         } else if (player === 'O') {
             board[square] = 'O';
-        } else return;
+        };
     }
 
     const clearBoard = function() {
@@ -31,46 +31,95 @@ const gameBoard = (function() {
 })();
 
 
+
 const Player = function(shape) {
-    if (!(shape === 'X' || shape === 'O')) return; 
-    return {shape};
+    let active = false;
+    let boardShape = shape;
+
+    if (!(boardShape === 'X' || boardShape === 'O')) return false; 
+
+    const getShape = function() {
+        return boardShape;
+    }
+
+    const getActive = function() {
+        return active;
+    }
+
+    const changeActive = function() {
+        active = !active; 
+    }
+
+    const checkIfWin = function() {
+        let board = gameBoard.getBoardState();
+        let x = boardShape;
+        let win = false;
+        if ((board[0] === x && board[1] === x && board[2] === x)) win = true;
+        if ((board[3] === x && board[4] === x && board[5] === x)) win = true;
+        if ((board[6] === x && board[7] === x && board[8] === x)) win = true;
+        if ((board[0] === x && board[3] === x && board[6] === x)) win = true;
+        if ((board[1] === x && board[4] === x && board[7] === x)) win = true;
+        if ((board[2] === x && board[5] === x && board[8] === x)) win = true;
+        if ((board[0] === x && board[4] === x && board[8] === x)) win = true;
+        if ((board[2] === x && board[4] === x && board[6] === x)) win = true;
+        return win;
+    }
+        
+    return {getShape, getActive, changeActive, checkIfWin};
 };
+
 
 
 const gameController = (function() {
     let player1, player2;
-    let winner = 'none';
-    let currentPlayer;
 
-
-    for (let i = 0; i < gameBoard.getBoardState; i++) {
-        let x = `p${i}`;
-        box = document.querySelector(x);
-        box.addEventListener('click', () => {
-            
-        });
-    }
-    
-
-    const enterPlayers = function(p1, p2) {
+    const addPlayers = function(p1, p2) {
         player1 = p1;
         player2 = p2;
+        player1.changeActive();
+    }
+
+    const switchCurrent = function() {
+        player1.changeActive();
+        player2.changeActive();
+    }
+
+    const getCurrentPlayer = function() {
+        if (player1.getActive() === true) return player1;
+        else return player2;
+    }
+
+    const checkForWinner = function() {
+        if (player1.checkIfWin() === true) return 1;
+        else if (player2.checkIfWin() === true) return 2;
+        else return 0;
+    }
+
+    for (let i = 0; i < gameBoard.getBoardState().length; i++) {
+        let id = `#s${i}`;
+        let ele = document.querySelector(id);
+        ele.addEventListener('click', function() {clickOnSquare(gameController.getCurrentPlayer().getShape(), i)});
     };
 
-    coinFlip = Math.floor(Math.random()*2) + 1;
-    if (coinFlip === 1) currentPlayer = player1;
-    else currentPlayer = player2;
-
-    let j = 0;
-    while (winner === 'none' && j < 9) {
-        //player picks square
-        //check for a winner
-        if (currentPlayer === player1) currentPlayer = player2;
-        else currentPlayer = player1;
-        j++;
+    function clickOnSquare(currentPlayer, squareNum) {
+        x = gameBoard.pickSquare(currentPlayer, squareNum);
+        console.log(x);
+        if (x !== false) {
+            updateWebpage.updateBoard(gameBoard.getBoardState());
+            gameController.switchCurrent();
+        };
     }
-    return {enterPlayers};
+
+    function coinFlip() {
+        result = Math.floor(Math.random()*2) + 1;
+        if (result === 1) return true;
+        else return false;
+    }
+
+    return {addPlayers, getCurrentPlayer, switchCurrent, checkForWinner}
 })();
+
+
 
 let updateWebpage = (function() {
     const updateBoard = function(currentBoard) {
@@ -82,3 +131,4 @@ let updateWebpage = (function() {
     }
     return {updateBoard};
 })();
+
